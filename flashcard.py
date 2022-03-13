92c7fb98708fe0c5d3a54c2c37bf79f42deb3444
ericdf <fackelman.eric@gmail.com>
2022-03-13 12:51:43 +0100

    Command Line version works! It queries for the translation into 2 languages and outputs correct if user is wrong twice.

--- flashcard.py
+++ flashcard.py
@@ -4,11 +4,48 @@ import numpy as np
 
 #readcsv(filename,columns,datatype)
 filename = "languages.csv"
-data = np.genfromtxt(filename,delimiter=',',dtype="U20")
-print(data)
+data = np.genfromtxt(filename,delimiter=',',dtype="U30")
+print(np.shape(data))
 
-
-#Randomly selects line and stores the words into output variables
+question1 = 'What is the word in ' + data[0,1] +'? '
+question2 = 'What is the word in ' + data[0,2] +'? '
+guess = 0
+#While input is not a quit variable, print random index
+while guess !='q':
+  index = np.random.randint(0,len(data))
+  print('The word in English is: ',data[index,0])
+  guess = input(question1)
+  if guess == 'q':
+    break
+  elif guess == data[index,1]:
+    print('Great job! You are correct!')
+  else:
+    guess = input('Sorry, try again: ')
+    if guess == 'q':
+      break
+    elif guess == data[index,1]:
+      print('Great job! You are correct!')
+      #continue
+    else:
+      print('Sorry, the correct word is: ',data[index,1],'. Next language!')
+      #continue
+    #continue
+  #Now try Norwegian
+  guess = input(question2)
+  if guess == 'q':
+    break
+  elif guess == data[index,2]:
+    print('Great job! You are correct!')
+  else:
+    guess = input('Sorry, try again: ')
+    if guess == 'q':
+      break
+    elif guess == data[index,2]:
+      print('Great job! You are correct!')
+      #continue
+    else:
+      print('Sorry, the correct word is: ',data[index,2],'. Next word!')
+      #continue
 
 #Displays first language of word at random and creates buttons to display others
 
